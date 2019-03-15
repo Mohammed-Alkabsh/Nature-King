a7c155481044b9f74bf3e51b10697c5f918c7f1b
Mohammed Alkabsh <alkabsh_mohammed@yahoo.com>
2019-03-15 19:34:14 +0000

    something is wrong

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
             $(".navs-container").css("display", "none");
             $(".navs-container").removeClass("fadeIn");
         }
-        if( $(window).height() <= $(window).width() ){
+        if( $(window).height() <= $(window).width() && $(window).width() <= 930){
             $(".nav-one-child").css("display", "flex");
             $(".nav-items").css("display", "inline-block");
         }else{
